11f1801156375d57a387ccfe89789b4175cc4a39
longetka <longetka@gmail.com>
2020-07-18 12:35:47 +0700

    Updates

---
+++ precache-manifest.2afb958c6f941710ee3b037645bb5635.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21186043b2bd557ffe19913941eca49",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ef965181c7ab1b4759c4",
+    "url": "/react-calculator/static/css/main.1dbe2909.chunk.css"
+  },
+  {
+    "revision": "dcd33b406fbd6a1cc970",
+    "url": "/react-calculator/static/js/2.42795050.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-calculator/static/js/2.42795050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef965181c7ab1b4759c4",
+    "url": "/react-calculator/static/js/main.1e80ae42.chunk.js"
+  },
+  {
+    "revision": "ec10e82e6cfd3ead7770",
+    "url": "/react-calculator/static/js/runtime-main.d01d9399.js"
+  },
+  {
+    "revision": "0ff4d1316d3e0af75c5f86011fabc3d4",
+    "url": "/react-calculator/static/media/mail.0ff4d131.png"
+  },
+  {
+    "revision": "d19bbdb4c119b7d984414179907a58d1",
+    "url": "/react-calculator/static/media/pngkey.com-github-icon-png-1787243.d19bbdb4.png"
+  },
+  {
+    "revision": "eb1fa44bd3e279f3f0b7c05df3bee9b9",
+    "url": "/react-calculator/static/media/telegram.eb1fa44b.png"
+  }
+]);
